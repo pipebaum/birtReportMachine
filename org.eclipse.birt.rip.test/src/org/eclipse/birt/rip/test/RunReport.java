b88885b568e4defb8176bcedb39e305b4865730f
steve <steve@stephenschafer.com>
2015-02-03 15:55:48 -0700

    First working version including /report/upload (POST), /report/download (GET), /report/run (POST).
    Added project org.eclipse.birt.rip.test to provide simple clients
    for testing.

---
+++ org.eclipse.birt.rip.test/src/org/eclipse/birt/rip/test/RunReport.java
@@ -0,0 +1,43 @@
+package org.eclipse.birt.rip.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RunReport {
+
+	public static void main(final String[] args) throws IOException {
+		final String fileIdString = args[0];
+		final URL url = new URL(
+				"http://localhost:8080/org.eclipse.birt.rip/birt/run/report/run/html/"
+						+ fileIdString);
+		final HttpURLConnection connection = (HttpURLConnection) url
+				.openConnection();
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "application/json");
+		final OutputStream outputStream = connection.getOutputStream();
+		try {
+			final byte[] outputBytes = "{}".getBytes();
+			outputStream.write(outputBytes);
+		} finally {
+			outputStream.close();
+		}
+		final String contentType = connection.getContentType();
+		System.out.println(contentType);
+		final InputStream inputStream = connection.getInputStream();
+		try {
+			final byte[] buffer = new byte[0x1000];
+			int bytesRead = inputStream.read(buffer);
+			while (bytesRead >= 0) {
+				System.out.write(buffer, 0, bytesRead);
+				bytesRead = inputStream.read(buffer);
+			}
+		} finally {
+			inputStream.close();
+		}
+	}
+
+}
--- org.eclipse.birt.rip/src/main/java/org/eclipse/birt/rest/resource/BirtEngineResource.java
+++ org.eclipse.birt.rip/src/main/java/org/eclipse/birt/rest/resource/BirtEngineResource.java
@@ -1,17 +1,33 @@
 package org.eclipse.birt.rest.resource;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
 
-import org.eclipse.birt.core.exception.BirtException;
+import net.sf.json.JSONObject;
+
 import org.eclipse.birt.engine.ReportEngine;
 import org.eclipse.birt.report.engine.api.EngineException;
 import org.eclipse.birt.report.engine.api.HTMLRenderOption;
@@ -22,58 +38,136 @@ import org.eclipse.birt.report.engine.api.RenderOption;
 
 @Path("run")
 public class BirtEngineResource {
+	private final File resourceDir;
+
+	public BirtEngineResource() {
+		final Properties properties = System.getProperties();
+		final String resourceDirName = properties
+				.getProperty("org.eclipse.birt.rip.resource.dir");
+		resourceDir = new File(resourceDirName);
+	}
+
+	@POST
+	@Path("/report/upload")
+	@Consumes({ MediaType.APPLICATION_OCTET_STREAM })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String uploadReport(final java.io.Reader reader) throws IOException {
+		final UUID uuid = UUID.randomUUID();
+		resourceDir.mkdirs();
+		final File file = new File(resourceDir, uuid.toString());
+		final FileWriter writer = new FileWriter(file);
+		try {
+			final char[] buffer = new char[0x1000];
+			int charsRead = reader.read(buffer);
+			while (charsRead >= 0) {
+				writer.write(buffer, 0, charsRead);
+				charsRead = reader.read(buffer);
+			}
+		} finally {
+			writer.close();
+		}
+		final Map<String, Object> outputMap = new HashMap<>();
+		outputMap.put("fileId", uuid.toString());
+		final JSONObject jsonObject = JSONObject.fromObject(outputMap);
+		return jsonObject.toString();
+	}
 
 	@GET
-	@Produces( { MediaType.TEXT_HTML } )
-	public String runReport () {
-		System.out.println("run a report");
-		IReportEngine reportEngine;
-		try {
-			reportEngine = ReportEngine.getReportEngine();
-			File testFile = new File("fud.xml");
-			
-			File designFile = new File("script.RPTDESIGN");
-			System.out.println("design file: " + designFile.getAbsolutePath());
+	@Path("/report/download/{fileId}")
+	@Produces({ MediaType.APPLICATION_OCTET_STREAM })
+	public StreamingOutput downloadReport(
+			@PathParam("fileId") final String fileIdString) {
+		final File file = new File(resourceDir, fileIdString);
+		return new StreamingOutput() {
 
-			final FileInputStream fis = new FileInputStream(designFile);
-			final IReportRunnable design = reportEngine.openReportDesign(fis);
-
-			System.out.println("Start run");
-
-			final IRunAndRenderTask runTask = reportEngine.createRunAndRenderTask(design);
-
-			runTask.validateParameters();
-
-			runTask.setRenderOption(getRenderOption());
-
-			System.out.println("Building " );
-			runTask.run();
-			@SuppressWarnings("unchecked")
-			List<EngineException> errors = (List<EngineException>)runTask.getErrors();
-			for (EngineException engineException : errors) {
-				System.out.println("ERROR:\t" + engineException.getMessage());
+			@Override
+			public void write(final OutputStream output) throws IOException,
+					WebApplicationException {
+				final FileInputStream fis = new FileInputStream(file);
+				try {
+					final byte[] buffer = new byte[0x1000];
+					int bytesRead = fis.read(buffer);
+					while (bytesRead >= 0) {
+						output.write(buffer, 0, bytesRead);
+						bytesRead = fis.read(buffer);
+					}
+				} finally {
+					fis.close();
+				}
 			}
-			
-			return runTask.getRenderOption().getOutputStream().toString();
-		} catch (IOException e) {
-			System.out.println("IOException" + e.getMessage());
-			e.printStackTrace();
-		} catch (BirtException e) {
-			System.out.println("BirtException" + e.getMessage());
-			e.printStackTrace();
+		};
+	}
+
+	@POST
+	@Path("/report/run/{outputFormat}/{fileId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.TEXT_HTML })
+	public StreamingOutput runReport(final String inputJsonString,
+			@PathParam("outputFormat") final String outputFormat,
+			@PathParam("fileId") final String fileIdString) {
+		final JSONObject jsonObject = JSONObject.fromString(inputJsonString);
+		final Iterator<?> iterator = jsonObject.keys();
+		while (iterator.hasNext()) {
+			final Object object = iterator.next();
+			System.out.println(object);
 		}
-		
-		
-		return "<html><body>Something went wrongs</body></html>";
+		// jsonObject holds the report parameters, if any
+		final File file = new File(resourceDir, fileIdString);
+		final UUID outputFileId = UUID.randomUUID();
+		final File outputFile = new File(resourceDir, outputFileId.toString()
+				+ "." + outputFormat);
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(outputFile);
+		} catch (final FileNotFoundException e) {
+			throw new ServerErrorException(500, e);
+		}
+		IReportEngine reportEngine;
+		reportEngine = ReportEngine.getReportEngine();
+		IRunAndRenderTask runTask;
+		try {
+			final FileInputStream fis = new FileInputStream(file);
+			final IReportRunnable design = reportEngine.openReportDesign(fis);
+			runTask = reportEngine.createRunAndRenderTask(design);
+			runTask.validateParameters();
+			final RenderOption options = new HTMLRenderOption();
+			options.setOutputFormat(outputFormat);
+			options.setOutputStream(fos);
+			runTask.setRenderOption(options);
+			runTask.run();
+			fos.close();
+		} catch (final FileNotFoundException e) {
+			throw new NotFoundException(e);
+		} catch (final IOException e) {
+			throw new ServerErrorException(500, e);
+		} catch (final EngineException e) {
+			throw new ServerErrorException(500, e);
+		}
+		@SuppressWarnings("unchecked")
+		final List<EngineException> errors = runTask.getErrors();
+		for (final EngineException engineException : errors) {
+			System.out.println("ERROR:\t" + engineException.getMessage());
+		}
+		if (!errors.isEmpty())
+			throw new ServerErrorException(errors.size()
+					+ " error(s) encountered.  See log for details.", 500);
+		return new StreamingOutput() {
+
+			@Override
+			public void write(final OutputStream output) throws IOException,
+					WebApplicationException {
+				final FileInputStream fis = new FileInputStream(outputFile);
+				try {
+					final byte[] buffer = new byte[0x1000];
+					int bytesRead = fis.read(buffer);
+					while (bytesRead >= 0) {
+						output.write(buffer, 0, bytesRead);
+						bytesRead = fis.read(buffer);
+					}
+				} finally {
+					fis.close();
+				}
+			}
+		};
 	}
-	
-	private RenderOption getRenderOption(){
-		RenderOption options = new HTMLRenderOption();
-		options.setOutputFormat(RenderOption.OUTPUT_FORMAT_HTML);
-		options.setOutputStream(new ByteArrayOutputStream());
-		
-		return options;
-		
-	}
-	
 }
